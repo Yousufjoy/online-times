ce01e032e1a3eceaf97f503638cfe915d64c878f
Yousufjoy <kanekiken333@gmail.com>
2023-10-06 23:00:35 +0600

    Custom tailwind class and font setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDj3-Xiefu5ChPTS6zeT5H0udp5BygZN8Y",
+  authDomain: "daily-times-3c827.firebaseapp.com",
+  projectId: "daily-times-3c827",
+  storageBucket: "daily-times-3c827.appspot.com",
+  messagingSenderId: "478625194305",
+  appId: "1:478625194305:web:6af86a2f168eaff9a081c0",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import Header from "../Shared/Header/Header";
+import Navbar from "../Shared/Navbar/Navbar";
+
+const Home = () => {
+  return (
+    <div>
+      <Header></Header>
+      <Navbar></Navbar>
+      <h2 className=" text-6xl border">This is home</h2>
+    </div>
+  );
+};
+
+export default Home;

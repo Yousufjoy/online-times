82937b78d09dbb98009a8d9c2edb7491c8d82764
Yousufjoy <kanekiken333@gmail.com>
2023-10-06 23:22:31 +0600

    Homepage layout and date time formatting using moment.js

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,7 +1,14 @@
+import logo from "../../../assets/logo.png";
+import moment from "moment";
+
 const Header = () => {
   return (
     <div>
-      <h2 className=" text-3xl"> Header</h2>
+      <img src={logo} alt="" />
+      <p>Journalism without fear or Favor</p>
+      <p className=" text-xl">
+        {moment().format("dddd, MMMM D, YYYY, h:mm:ss a")}{" "}
+      </p>
     </div>
   );
 };
